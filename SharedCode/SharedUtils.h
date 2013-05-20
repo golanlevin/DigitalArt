d3d9a6913ba03aef56af963ed198ce3744f31c70
Kyle McDonald <kyle@kylemcdonald.net>
2013-05-20 16:11:43 -0400

    cross platform data path

--- SharedCode/SharedUtils.h
+++ SharedCode/SharedUtils.h
@@ -4,6 +4,7 @@ void sharedSetup() {
 	ofSetVerticalSync(true);
 	ofSetFrameRate(120);
 	ofSetLogLevel(OF_LOG_VERBOSE);
-	ofSetCircleResolution(32); 
-	ofSetDataPathRoot("../../../../../SharedData/");
+	ofSetCircleResolution(32);
+	string basePath = ofToDataPath("", true);
+	ofSetDataPathRoot(basePath + "../../../SharedData/");
 }
