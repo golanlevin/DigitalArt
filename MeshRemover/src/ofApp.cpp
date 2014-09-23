3f1fe5f695c773ec649d7b191c34639e959dff46
Kyle McDonald <kyle@kylemcdonald.net>
2014-09-23 14:02:30 -0400

    updating project files, corrected the MeshRemover for new mesh

--- MeshRemover/src/ofApp.cpp
+++ MeshRemover/src/ofApp.cpp
@@ -10,38 +10,34 @@ void ofApp::setup() {
 	showWireframe = true;
 	showRemoval = true;
 	
-	hand.loadImage("hand/genericHandCentered.jpg");
-	mesh.load("hand/handmarks.ply");
+	hand.loadImage("hand/genericHandCenteredNew.jpg");
+	mesh.load("hand/handmarksNew.ply");
 	for(int i = 0; i < mesh.getNumVertices(); i++) {
 		mesh.addTexCoord(mesh.getVertex(i));
 	}
 	
+    // step 1: build these points
 	removalRegion.close();
-	int toRemove[] = {
-		293, 363, 364, 365,
-		366, 367, 368, 289,
-		176, 181, 186, 191,
-		196, 201, 206, 211,
-		216, 221, 226, 229,
-		228, 227, 222, 217,
-		212, 207, 202, 197,
-		192, 187, 182, 177,
-		119, 288, 362, 361,
-		360, 359, 358, 357
+	int toRemove[] = {176, 290, 374, 373, 372, 371, 370, 369, 293, 363, 364, 365, 366, 367, 368, 289, 119, 177, 182, 187, 192, 197, 202, 207, 212, 217, 222, 227, 228, 229, 226, 221, 216, 211, 206, 201, 196, 191, 186, 181
 	};
 	int toRemoveCount = 40;
 	for(int i = 0; i < toRemoveCount; i++) {
 		removalRegion.addVertex(mesh.getVertex(toRemove[i]));
-	}
+    }
+    
+    // step 2: remove the triangles for the remaining indices
+//    removeTriangles(mesh, removalRegion);
+//    mesh = dropUnusedVertices(mesh);
 	
 	// post-removal indices, not original indices
-	int toStitchLeft[] = {307, 308, 309, 310, 311, 312, 233, 173};
-	int toStitchRight[] = {301, 302, 303, 304, 305, 306, 232, 119,};
+    int toStitchLeft[] = {173, 234, 318, 317, 316, 315, 314, 313};
+    int toStitchRight[] = {119, 233, 312, 311, 310, 309, 308, 307};
 	int toStitchCount = 8;
 	for(int i = 0; i < toStitchCount; i++) {
 		stitch.push_back(pair<ofIndexType, ofIndexType>(toStitchLeft[i], toStitchRight[i]));
 	}
-	
+    
+    // step 3: remove and stitch
 	mesh = removeAndStitch(mesh, removalRegion, stitch);
 	
 	puppet.setup(mesh);
